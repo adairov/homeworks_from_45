4ca16b691c3e2c50448587b96de85b308dbf0d76
adairov <aibek.dairov@gmail.com>
2022-06-28 05:19:24 +0600

    db created commit

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ToDoList(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=100, null=False, blank=False, verbose_name="Статус", default="New")
+    deadline = models.DateField(verbose_name="Дедлайн")
+
+    def __str__(self):
+        return f"{self.id}. {self.title}:{self.status}"
