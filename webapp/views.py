bf723d9b0c82f77245d9f5bb195dfb296700166b
adairov <aibek.dairov@gmail.com>
2022-06-28 08:42:34 +0600

    create_task page created commit

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from webapp.models import ToDoList
+
+
 def index_view(request):
-    return render(request, 'index.html')
+    plans = ToDoList.objects.all()
+    context = {"plans": plans}
+    return render(request, 'index.html', context)
+
+def create_task(request):
+    return render(request, 'create_task.html')
