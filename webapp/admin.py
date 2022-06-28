1095b4c717be7db19ff7011ecada2a4ef7ebc543
adairov <aibek.dairov@gmail.com>
2022-06-28 06:08:12 +0600

    admins.py configured commit

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import ToDoList
+
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'status', 'deadline']
+    list_display_links = ['title']
+    list_filter = ['status']
+    search_fields = ['title', 'status']
+    fields = ['title', 'status', 'deadline']
+    readonly_fields = ['deadline']
+
+admin.site.register(ToDoList, ToDoListAdmin)
